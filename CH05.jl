da83f15eb3683dbb0028551f42265a7504d94ebc
ilkerarslan <ilker.arslan@outlook.com>
2023-01-15 21:48:33 +0300

    chapter 5 added

--- CH05.jl
+++ CH05.jl
@@ -65,7 +65,6 @@ data.default = default;
 data[!, end-2:end]
 
 # Convert nonnumerical data to numerical
-
 coltypes = DataFrame(column=names(data), 
                      type=eltype.(eachcol(data)))
 
@@ -113,7 +112,6 @@ column2onehot!(data, "custSegment")
 data[:, [:Micro, :Medium, :Commercial, :Corporate]]
 data[:, :custSegment]  # should throw an error.
 
-
 # Label encoding
 column = data.creditRating;
 
@@ -135,6 +133,11 @@ select!(data, Not("creditRating"))
 # Numerical columns
 data
 describe(data)
+
+show(describe(data), allrows=true)
+data |> describe |> df -> show(df, allrows=true)
+
+data = data[!, Not(:minOverdueDays)];
 
 coltypes = eltype.(eachcol(data))
 numtypes = (coltypes .<: Real)  .| (coltypes .<: Union{Missing, Real})
@@ -184,5 +187,125 @@ data.lastCreditUsageDate = medianImpute(data.lastCreditUsageDate)
 
 # Outliers
 
+describe(data.undueCheckCount)
+
+using Plots, StatsPlots
+histogram(data.undueCheckCount, bins=:scott, legend=false)
+boxplot(data.undueCheckCount, legend=false, xaxis=[])
+
+using Statistics
+q1, q3 = quantile(data.undueCheckCount, [0.25, 0.75])
+iqr = q3 - q1
+max_thresh = q3 + 1.5 * iqr
+
+data[!, :undueCheckCount] = data[!, :undueCheckCount] .|> v -> (v > max_thresh ? max_thresh : v)
+
+
+# Standardize and Scale Data
 data |> describe |> df -> show(df, allrows=true)
 
+histogram(data.totalLoanExposure, bins=:scott, legend=false)
+
+logvalues = log.(data.totalLoanExposure .+ 1)
+histogram(logvalues, bins=:scott, legend=false)
+
+avg = mean(logvalues)
+sd = std(logvalues)
+
+stnorm = (logvalues .- avg) ./ sd;
+histogram(stnorm, bins=:scott, legend=false)
+
+describe(stnorm)
+
+stnorm = stnorm .|> x -> x < -3 ? -3 : (x > 3 ? 3 : x) 
+
+histogram(data.paidCheckAmount)
+
+minmaxscaler(x) = (x .- minimum(x)) ./ (maximum(x) - minimum(x))
+scaled = minmaxscaler(data.paidCheckAmount)
+describe(scaled)
+
+describe(data) |> df -> show(df, allrows=true)
+
+function stdnormalizer(x)
+    avg = x |> skipmissing |> mean
+    sd = x |> skipmissing |> std
+    return (x .- avg) ./ sd
+end
+
+for i ∈ 4:71
+    data[!, i] = stdnormalizer(data[!, i])
+end
+
+describe(data, :detailed, ) |> df -> show(df, allrows=true)
+
+# Find correlations
+using Statistics
+
+function corrcoef(x,y)    
+    indices = .!(ismissing.(x) .| ismissing.(y))
+    x = x[indices]
+    y = y[indices]
+    mx = mean(x)
+    my = mean(y)
+
+    sdx = std(x)
+    sdy = std(y)
+
+    n = length(x)
+
+    covxy = (x .- mx) .* (y .- my) |> sum 
+    covxy = covxy / (n-1)
+
+    return covxy /(sdx * sdy)
+end
+
+a = data.totalCashLoanLimit
+b = data.totalLoanLimit
+corrcoef(a,b)
+
+variables = names(data)[5:71]
+corrdf = DataFrame(var1=String[], var2=String[], corrcoef=Float64[])
+
+for i in 1:length(variables)-1
+    for j in (i+1):length(variables)
+        var1 = variables[i]
+        var2 = variables[j]
+        coef = corrcoef(data[:, var1], data[:, var2])
+        push!(corrdf, (var1=var1, var2=var2, corrcoef=coef))
+    end
+end
+
+size(corrdf)
+corrdf[!, :absCorrCoef] = abs.(corrdf[!, :corrcoef]);
+sort!(corrdf, :absCorrCoef, rev=true)
+
+extravars = subset(corrdf, :absCorrCoef => x -> x .> 0.9)
+extravars.var2 |> unique
+
+cols2remove = unique(extravars.var2)
+data = data[!, Not(cols2remove)]
+
+
+
+using DataFrames, Plots
+
+function split_to_bins(column::Symbol, n_bins::Int)
+    df = data[:, [column, :default]]
+    sort!(df, column)
+    df[!, :bin] .= 0
+    binlimits = (nrow(df) ÷ n_bins) .* (1:(n_bins-1)) |> collect
+    pushfirst!(binlimits, 1)
+    
+    for i ∈ 1:(n_bins-1)
+        df[binlimits[i]:binlimits[i+1], :bin] .= i
+    end
+    
+    df[binlimits[end]:nrow(df), :bin] .= n_bins;
+    res = combine(groupby(df, :bin), :default => mean)
+    plot(res.bin, res.default_mean, legend=false, title=column)
+    scatter!(res.bin, res.default_mean, legend=false)
+end
+
+split_to_bins(:maxOverdueDays, 3)
+split_to_bins(:lastBounceDate, 4)
